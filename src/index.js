1575b0ce76e34e3d65ab6fb8d0f9bb72428ad941
Stanislav Sheinoha <stanyslav.sheinoha@gmail.com>
2023-01-26 21:46:49 +0200

    Updates the code of some files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './css/styles.css';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
 
-const DEBOUNCE_DELAY = 300;
+const searchForm = document.querySelector('.search-form');
+console.log(searchForm);
+
+searchForm.addEventListener('submit', http);
+
+function http(event) {
+  event.preventDefault();
+  console.log("Sorry, server can't answer");
+}
